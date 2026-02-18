247a6364dcaf84fb561a6cc30aab7bec91cc8609
MikhSuv <mischasuv@mail.ru>
2026-02-18 20:14:51 +0300

    Added matrix multiplication function

--- app/main.f90
+++ app/main.f90
@@ -1,18 +1,17 @@
 program main
   use precision_mod
-  use matrix_multiplication, only: say_hello
+  use matrix_multiplication
   use matrixio
   implicit none
 
-  real(dp), allocatable :: matrix(:,:) ! квадратная матрица n*n
+  real(dp), allocatable :: a(:,:), b(:,:), c(:,:) ! квадратная матрица n*n
   integer :: n
 
-  call read_matrix("data1.dat", matrix, n)
-  write(*,*) n
-  call say_hello()
-  write(*,*) matrix
-  call write_matrix("result.dat", matrix, n)
+  call read_matrix("data1.dat", a, n)
+  call read_matrix("data2.dat", b, n)
+  c = matrix_mult(a, b, n)
+  call write_matrix("result.dat", c, n)
 
-  deallocate(matrix)
+  deallocate(a, b, c)
 
 end program main
--- src/matrix_multiplication.f90
+++ src/matrix_multiplication.f90
@@ -1,10 +1,28 @@
 module matrix_multiplication
+  use precision_mod
   implicit none
   private
 
-  public :: say_hello
+  public :: matrix_mult
 contains
-  subroutine say_hello
-    print *, "Hello, matrix_multiplication!"
-  end subroutine say_hello
+
+  function matrix_mult(first_matrix, second_matrix, n) result(prod) ! TODO: исключить n из аргументов
+    ! Вычисляет произведение квадратных матриц 
+    real(dp), dimension(n, n), intent(in) :: first_matrix, second_matrix 
+    integer, intent(in) :: n
+    real(dp) :: prod(n,n)
+
+    integer :: i, j, k
+
+    do i = 1, n
+      do j = 1, n 
+        prod(j, i) = 0
+        do k = 1, n
+          prod(j, i) = prod(j, i) + second_matrix(j, k)*first_matrix(k, i)
+        end do
+      end do
+    end do
+    
+  end function matrix_mult
+
 end module matrix_multiplication
