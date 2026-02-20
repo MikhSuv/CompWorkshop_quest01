88f2c64c2dab4c5e3e746528d527744716f5b5b0
MikhSuv <mischasuv@mail.ru>
2026-02-20 11:28:04 +0300

    Added random test

--- test/check.f90
+++ test/check.f90
@@ -1,5 +1,25 @@
 program check
+  use precision_mod
+  use matrix_multiplication
+  use matrixio
+
 implicit none
 
-print *, "Put some tests in here!"
+real(dp), dimension(:, :), allocatable :: A, B, C,D
+integer, parameter :: n = 4096
+
+allocate(A(n,n))
+allocate(B(n,n))
+
+call random_number(A)
+print *, "Сгенерил матрицу А"
+call random_number(B)
+print *, "Сгенерил матрицу B"
+C = matmul(A, B)
+D = matmul_opt(a, b,n)
+print *, "matrix_mult ВСЁ"
+D = abs(D -C)
+print *, maxval(D)
+deallocate(a,b,c,d)
+
 end program check
