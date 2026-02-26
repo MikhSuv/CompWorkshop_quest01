a17f0559c520fecb2ffd04f1a3445234a4af06b9
MikhSuv <mischasuv@mail.ru>
2026-02-27 00:54:43 +0300

    Ошибка на глваной диагонали

--- part2/app/main.f90
+++ part2/app/main.f90
@@ -4,8 +4,17 @@ program main
 
   implicit none
   
-  type(tridiag_matrix) :: A
+  type(tridiag_matrix) :: A, B
+  type(pentadiag_matrix) :: C
+  ! integer, parameter :: n = 10
 
-  call A%read_tdmatrix('data1.txt')
-  call A%print_matrix()
+  call A%read_tdmatrix('data1.dat')
+  call B%read_tdmatrix ('data2.dat')
+  ! call A%generate(n)
+  ! call B%generate(n)
+  call A%print_matrix
+  call B%print_matrix()
+  C = tdmatmul(A, B)
+  call C%write_pdmatrix('result.dat')
+
 end program main
--- part2/src/tridiagonal_matrix.f90
+++ part2/src/tridiagonal_matrix.f90
@@ -3,9 +3,9 @@ module tridiagonal_matrix
   use precision_mod
   implicit none
   private
-  public :: tridiag_matrix, read_tdmatrix, print_matrix
+  public :: tdmatmul
 
-  type :: tridiag_matrix
+  type, public :: tridiag_matrix
     private
     integer :: n = 0
     real(dp), allocatable :: main_diag(:) ! Главная диагональ
@@ -14,8 +14,18 @@ module tridiagonal_matrix
   contains
     procedure :: read_tdmatrix 
     procedure :: print_matrix
+    procedure :: generate
     ! procedure :: tdmatmul ! TODO: OOP
   end type tridiag_matrix
+
+  type, public :: pentadiag_matrix
+    private
+    integer :: n = 0
+    real(dp), allocatable :: diags(:, :) !матрица 5*n, 5 строк -2, -1, 0, 1, 2 диагонали
+  contains
+     procedure :: write_pdmatrix
+
+  end type pentadiag_matrix
 
 
   contains
@@ -41,7 +51,7 @@ module tridiagonal_matrix
       if (line(1:1) == "#") line = line(2:) ! пропуск '#'
       read(line, *) matrix%n ! чтение порядка матрицы
 
-      allocate(matrix%main_diag(matrix%n))
+      allocate(matrix%main_diag(matrix%n)) ! Выделение памяти под диагонали
       allocate(matrix%lower_diag(matrix%n-1))
       allocate(matrix%upper_diag(matrix%n-1))
 
@@ -73,6 +83,84 @@ module tridiagonal_matrix
       print *, 'Нижняя', matrix%lower_diag
       print *, 'Верхняя', matrix%upper_diag
     end subroutine print_matrix
+
+    subroutine generate(matrix, n)
+      ! Заполняет матрицу случайными числами из [0,1]
+      class(tridiag_matrix), intent(out) :: matrix
+      integer, intent(in) :: n
+      if (n <= 0) error stop 'N have to be positive'
+      matrix%n = n
+      allocate(matrix%main_diag(n))
+      allocate(matrix%lower_diag(n-1))
+      allocate(matrix%upper_diag(n-1))
+      call random_number(matrix%main_diag)
+      call random_number(matrix%upper_diag)
+      call random_number(matrix%lower_diag)
+    end subroutine generate
+
+    function tdmatmul(A, B) result(C)
+      type(tridiag_matrix), intent(in) :: A,B
+      type(pentadiag_matrix) :: C
+
+      integer :: i, n
+      n = A%n
+      C%n = n
+      allocate(C%diags(5, n), source=0.0_dp)
+
+    ! Главная диагональ
+      do i = 1, n
+        C%diags(3, i) = A%main_diag(i) * B%main_diag(i)
+        if (i > 1) then
+            C%diags(3, i) = C%diags(3, i) + A%lower_diag(i-1) * B%upper_diag(i-1)
+        end if
+        if (i < n) then
+            C%diags(3, i) = C%diags(3, i) + A%upper_diag(i) * B%lower_diag(i+1)
+        end if
+      end do
+      ! Первая наддиагональ
+      do i = 1, n-1
+        C%diags(4, i) = A%main_diag(i) * B%upper_diag(i) + A%upper_diag(i) * B%main_diag(i+1)
+      end do
+      ! Вторая наддиагональ
+      do i = 1, n-2
+        C%diags(5, i) = A%upper_diag(i) * B%upper_diag(i+1)
+      end do
+      ! Первая поддиагональ
+      do i = 2, n
+        C%diags(2, i) = A%lower_diag(i-1) * B%main_diag(i-1) + A%main_diag(i) * B%lower_diag(i-1)
+      end do
+      ! Вторая поддиагональ
+      do i = 3, n
+        C%diags(1, i) = A%lower_diag(i-1) * B%lower_diag(i-2)
+      end do
+    end function tdmatmul
+
+    subroutine write_pdmatrix(matrix, filename)
+      class(pentadiag_matrix), intent(in) :: matrix
+      character(len=*), intent(in) :: filename
+      integer :: i, ounit, iostatus
+
+      open(newunit=ounit, file=filename, action='write', iostat=iostatus)
+      if (iostatus /= 0) then
+        error stop 'Error occured while opening file'
+      end if
+
+      write(ounit, '("# ", i0)') matrix%n
+do i = 1, matrix%n
+      if (i == 1) then
+        write(ounit, '(3(f12.6,1x))') matrix%diags(3:5, i)
+      else if (i == 2) then
+        write(ounit, '(4(f12.6,1x))') matrix%diags(2:5, i)
+      else if (i == matrix%n - 1) then
+        write(ounit, '(4(f12.6,1x))') matrix%diags(1:4, i)
+      else if (i == matrix%n) then
+        write(ounit, '(3(f12.6,1x))') matrix%diags(1:3, i)
+      else
+        write(ounit, '(5(f12.6,1x))') matrix%diags(1:5, i)
+      end if
+    end do
+      close(ounit)
+    end subroutine write_pdmatrix
 
 end module tridiagonal_matrix 
 
